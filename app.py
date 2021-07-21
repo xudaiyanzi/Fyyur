a520ba4ac59b52b209a0b3b6d282ab641c9526c5
Yan Xu <xudaiyanzi@gmail.com>
2021-07-21 10:52:27 -0500

    add the show page

--- app.py
+++ app.py
@@ -481,6 +481,7 @@ def create_artist_form():
 def create_artist_submission():
   # called upon submitting the new artist listing form
   # TODO: modify data to be the data object returned from db insertion
+ # Done!!!
 
   error = False
   try:
@@ -537,42 +538,32 @@ def shows():
   # displays list of shows at /shows
   # TODO: replace with real venues data.
   #       num_shows should be aggregated based on number of upcoming shows per venue.
-  data=[{
-    "venue_id": 1,
-    "venue_name": "The Musical Hop",
-    "artist_id": 4,
-    "artist_name": "Guns N Petals",
-    "artist_image_link": "https://images.unsplash.com/photo-1549213783-8284d0336c4f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80",
-    "start_time": "2019-05-21T21:30:00.000Z"
-  }, {
-    "venue_id": 3,
-    "venue_name": "Park Square Live Music & Coffee",
-    "artist_id": 5,
-    "artist_name": "Matt Quevedo",
-    "artist_image_link": "https://images.unsplash.com/photo-1495223153807-b916f75de8c5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80",
-    "start_time": "2019-06-15T23:00:00.000Z"
-  }, {
-    "venue_id": 3,
-    "venue_name": "Park Square Live Music & Coffee",
-    "artist_id": 6,
-    "artist_name": "The Wild Sax Band",
-    "artist_image_link": "https://images.unsplash.com/photo-1558369981-f9ca78462e61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=794&q=80",
-    "start_time": "2035-04-01T20:00:00.000Z"
-  }, {
-    "venue_id": 3,
-    "venue_name": "Park Square Live Music & Coffee",
-    "artist_id": 6,
-    "artist_name": "The Wild Sax Band",
-    "artist_image_link": "https://images.unsplash.com/photo-1558369981-f9ca78462e61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=794&q=80",
-    "start_time": "2035-04-08T20:00:00.000Z"
-  }, {
-    "venue_id": 3,
-    "venue_name": "Park Square Live Music & Coffee",
-    "artist_id": 6,
-    "artist_name": "The Wild Sax Band",
-    "artist_image_link": "https://images.unsplash.com/photo-1558369981-f9ca78462e61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=794&q=80",
-    "start_time": "2035-04-15T20:00:00.000Z"
-  }]
+  # DONE!!!
+
+  data = []
+  show_item = {}
+  
+  for show in Shows.query.all():
+      venue_id = Shows.query.filter_by(id=Shows.venue_id).id
+      venue_name = Venue.query.filter_by(id=Shows.venue_id).name
+
+      artist_id = Shows.query.filter_by(id=Shows.artist_id).id
+      artist_name = Artist.query.filter_by(id=Shows.artist_id).name
+      artist_image_link = Artist.query.filter_by(id=Shows.artist_id).image_link
+
+      start_time = Shows.query.filter_by(id=Shows.venue_id).start_time
+
+      show_item = {
+        "id": show.id,
+        "venue_id": venue_id,
+        "venue_name": venue_name,
+        "artist_id": artist_id,
+        "artist_name": artist_name,
+        "artist_image_link": artist_image_link,
+        "start_time": start_time
+      }
+      data.append(show_item)
+
   return render_template('pages/shows.html', shows=data)
 
 @app.route('/shows/create')
