02c910935f8aeca5511404b287c9f675064eaf88
Yan Xu <xudaiyanzi@gmail.com>
2021-07-21 23:35:43 -0500

    finish the edit 'get' method by using the form=XxForm(obj=venue or artist)

--- app.py
+++ app.py
@@ -410,26 +410,13 @@ def show_artist(artist_id):
 #  ----------------------------------------------------------------
 @app.route('/artists/<int:artist_id>/edit', methods=['GET'])
 def edit_artist(artist_id):
-  form = ArtistForm()
-  artist={
-    "id": 4,
-    "name": "Guns N Petals",
-    "genres": ["Rock n Roll"],
-    "city": "San Francisco",
-    "state": "CA",
-    "phone": "326-123-5000",
-    "website": "https://www.gunsnpetalsband.com",
-    "facebook_link": "https://www.facebook.com/GunsNPetals",
-    "seeking_venue": True,
-    "seeking_description": "Looking for shows to perform at in the San Francisco Bay Area!",
-    "image_link": "https://images.unsplash.com/photo-1549213783-8284d0336c4f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80"
-  }
-  # TODO: populate form with fields from artist with ID <artist_id>
 
-
-  # return render_template('forms/edit_artist.html', form=form, artist=artist)
-  return render_template('forms/edit_artist.html', form=form, artist_id=Artist.query.filter_by(id=artist_id).all()
-  )
+# TODO: populate form with fields from artist with ID <artist_id>
+# DONE!!!
+  artist = Artist.query.filter_by(id=artist_id).first()
+  form = ArtistForm(obj=artist)
+  
+  return render_template('forms/edit_artist.html', form=form, artist=artist)
 
 @app.route('/artists/<int:artist_id>/edit', methods=['POST'])
 def edit_artist_submission(artist_id):
@@ -440,25 +427,13 @@ def edit_artist_submission(artist_id):
 
 @app.route('/venues/<int:venue_id>/edit', methods=['GET'])
 def edit_venue(venue_id):
-  form = VenueForm()
-  venue={
-    "id": 1,
-    "name": "The Musical Hop",
-    "genres": ["Jazz", "Reggae", "Swing", "Classical", "Folk"],
-    "address": "1015 Folsom Street",
-    "city": "San Francisco",
-    "state": "CA",
-    "phone": "123-123-1234",
-    "website": "https://www.themusicalhop.com",
-    "facebook_link": "https://www.facebook.com/TheMusicalHop",
-    "seeking_talent": True,
-    "seeking_description": "We are on the lookout for a local artist to play every two weeks. Please call us.",
-    "image_link": "https://images.unsplash.com/photo-1543900694-133f37abaaa5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60"
-  }
-  # TODO: populate form with values from venue with ID <venue_id>
 
-  # return render_template('forms/edit_venue.html', form=form, venue=venue)
-  return render_template('forms/edit_venue.html', form=form, venue_id=venue_id)
+  # TODO: populate form with values from venue with ID <venue_id>
+  # DONE!!!
+  venue = Venue.query.filter_by(id=venue_id).first()
+  form = VenueForm(obj=venue)
+
+  return render_template('forms/edit_venue.html', form=form, venue=venue)
 
 @app.route('/venues/<int:venue_id>/edit', methods=['POST'])
 def edit_venue_submission(venue_id):
